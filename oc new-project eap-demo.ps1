266b7c1b13f5775a693a4e4e6bb8694d1d4eb38d
Parris Lucas <1635152+GrooveCS@users.noreply.github.com>
2020-09-08 15:44:49 -0400

    Added Local Environment Variable Functional Testing of Variables

--- oc new-project eap-demo.ps1
+++ oc new-project eap-demo.ps1
@@ -6,32 +6,38 @@
 
 # Login into OpenShift
 
+#Local Environment Variables
+$Env:PATH = "C:\Users\Developer\.crc\bin\oc;$Env:PATH"
+$CRC_PATH = "D:\Developer\CRC\"
+$EAP_HOME   = "D:\volume\sandbox\jboss-eap-7.3\"
+$EAP7_HOME  = "D:\volume\sandbox\jboss-eap-7.3\bin\"
+$JBOSS_HOME  = "D:\volume\sandbox\jboss-eap-7.3\"
 
-$OC-EAP-Project = 'eap-demo-02'
+#OpenShift Deployment Variables
+$OC_SECRET_PATH             = "$($CRC_PATH).\secrets\"
+$OC_EAP_PROJECT             = "eap-demo-02"
+$JBOSS_EAP_IMAGE_DIRECTORY  = "eap73-openjdk11"
+$JBOSS_EAP_IMAGE_URL        = "https://raw.githubusercontent.com/jboss-container-images/jboss-eap-7-openshift-image/$($JBOSS_EAP_IMAGE_DIRECTORY)/templates/"
 
-$JBOSS-EAP-Image-Directory = 'eap73-openjdk11'
-$JBOSS-EAP-Image-URL = 'https://raw.githubusercontent.com/jboss-container-images/jboss-eap-7-openshift-image/$JBOSS-EAP-Image-Directory/templates/'
-
-
-oc new-project $OC-EAP-Project
+oc new-project $OC_EAP_PROJECT
 
 # Adding registry.redhat.io secret
-oc create -f .\12666202_idm-odo-sa-secret --namespace=$OC-EAP-Project
-oc create -f .\12666202_parris-redhat-com-secret.yaml --namespace=$OC-EAP-Project
+oc create -f "$($OC_SECRET_PATH)12666202_idm-odo-sa-secret.yaml" --namespace=$OC_EAP_PROJECT
+oc create -f "$($OC_SECRET_PATH)12666202_parris-redhat-com-secret.yaml" --namespace=$OC_EAP_PROJECT
 #endregion
 
 #region Add missing templates jboss-eap-7-openshift-image/eap73-openjdk11
-oc replace --force -n $OC-EAP-Project -f $JBOSS-EAP-Image-Directoryeap73-openjdk11-image-stream.json
-oc replace --force -n $OC-EAP-Project -f $JBOSS-EAP-Image-Directoryeap73-openjdk11-amq-persistent-s2i.json 
-oc replace --force -n $OC-EAP-Project -f $JBOSS-EAP-Image-Directoryeap73-openjdk11-amq-s2i.json 
-oc replace --force -n $OC-EAP-Project -f $JBOSS-EAP-Image-Directoryeap73-openjdk11-basic-s2i.json 
-oc replace --force -n $OC-EAP-Project -f $JBOSS-EAP-Image-Directoryeap73-openjdk11-https-s2i.json 
-oc replace --force -n $OC-EAP-Project -f $JBOSS-EAP-Image-Directoryeap73-openjdk11-sso-s2i.json 
-oc replace --force -n $OC-EAP-Project -f $JBOSS-EAP-Image-Directoryeap73-openjdk11-third-party-db-s2i.json 
-oc replace --force -n $OC-EAP-Project -f $JBOSS-EAP-Image-Directoryeap73-openjdk11-tx-recovery-s2i.json
+oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-image-stream.json"
+oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-amq-persistent-s2i.json"
+oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-amq-s2i.json"
+oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-basic-s2i.json" 
+oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-https-s2i.json"
+oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-sso-s2i.json"
+oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-third-party-db-s2i.json"
+oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-tx-recovery-s2i.json"
 
 # import current OS Image from registry.redhat.io
-oc -n $OC-EAP-Project import-image registry.redhat.io/jboss-eap73-openshift-rhel8:TP --confirm
+oc -n $OC_EAP_PROJECT import-image registry.redhat.io/jboss-eap73-openshift-rhel8:TP --confirm
 
 <# An example Red Hat JBoss EAP 7 application. For more information about using this template, 
 see https://github.com/jboss-container-images/jboss-eap-7-openshift-image/blob/eap72/README.adoc #>
@@ -40,7 +46,8 @@ see https://github.com/jboss-container-images/jboss-eap-7-openshift-image/blob/e
 <# commented out to prevent running
 # Install basic EAP 7.x, openJDK 11 S2I on to OpenShift
 
-oc -n $OC-EAP-Project new-app eap73-openjdk11-basic-s2i -p APPLICATION_NAME=eap73-basic-app
+oc -n $OC_EAP_PROJECT new-app eap73-openjdk11-basic-s2i -p APPLICATION_NAME=eap73-basic-app
+
  #>
  
 
@@ -50,9 +57,10 @@ oc -n $OC-EAP-Project new-app eap73-openjdk11-basic-s2i -p APPLICATION_NAME=eap7
 # Powershell continue next line is "`" and replaced bash "\"
 
 oc new-app --template=eap73-openjdk11-basic-s2i `
- -p IMAGE_STREAM_NAMESPACE=$OC-EAP-Project `
+ -p IMAGE_STREAM_NAMESPACE=$OC_EAP_PROJECT `
  -p SOURCE_REPOSITORY_URL=https://github.com/jboss-developer/jboss-eap-quickstarts `
  -p SOURCE_REPOSITORY_REF=7.3.x-openshift `
  -p GALLEON_PROVISION_LAYERS=jaxrs-server `
  -p CONTEXT_DIR=helloworld-html5
+ 
  #>
