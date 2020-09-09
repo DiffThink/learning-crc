07c69943ac11833f1adfe9b540b7ed4847d9da08
Parris Lucas <1635152+GrooveCS@users.noreply.github.com>
2020-09-08 20:59:37 -0400

    Add *.txt to .gitignore Add container registries Add logins to registries Add variables to set cluster user, token, and context Add OC Secrets, JBOSS, and EAP paths Add JBoss EAP 7 registry.redhat.io URLs Add PostgreSQL 12 registry.redhat.io URLs Add hibernate quickstart `oc new-app` template command Add regions with labels to expand|collapse sections Add Registry Catalog to Resource List

--- oc new-project eap-demo.ps1
+++ oc new-project eap-demo.ps1
@@ -1,27 +1,47 @@
+<# 
+    Deploys the following to OpenShift
 
-#region New Project
+        1. Project
+        2. Applictaion
+        3. Add Templates
+ #>
 
-# Login into Hub.Docker.com, Quay.io, and Registry.RedHat.io
+#region Login into Hub.Docker.com, Quay.io, and Registry.RedHat.io
+
+docker login -u="plucas-redhat-com+parris_redhat_com" quay.io
+docker login --username parris4redhat #hub.docker.io
+docker login registry.access.redhat.com
+docker login registry.redhat.io 
+#endregion
+
+#region OpenShift Variables
+
+$OC_CURRENT_CLUSTER_USER = $(oc whoami)
+$OC_CURRENT_CLUSTER_TOKEN = $(oc whoami -t)
+$OC_CURRENT_CLUSTER_CONTEXT = $(oc config current-context)
+#endregion
 
 
-# Login into OpenShift
+#region Local Environment Variables
 
-#Local Environment Variables
 $Env:PATH = "C:\Users\Developer\.crc\bin\oc;$Env:PATH"
 $CRC_PATH = "D:\Developer\CRC\"
 $EAP_HOME   = "D:\volume\sandbox\jboss-eap-7.3\"
 $EAP7_HOME  = "D:\volume\sandbox\jboss-eap-7.3\bin\"
 $JBOSS_HOME  = "D:\volume\sandbox\jboss-eap-7.3\"
+#endregion
 
-#OpenShift Deployment Variables
-$OC_SECRET_PATH             = "$($CRC_PATH).\secrets\"
-$OC_EAP_PROJECT             = "eap-demo-02"
-$JBOSS_EAP_IMAGE_DIRECTORY  = "eap73-openjdk11"
-$JBOSS_EAP_IMAGE_URL        = "https://raw.githubusercontent.com/jboss-container-images/jboss-eap-7-openshift-image/$($JBOSS_EAP_IMAGE_DIRECTORY)/templates/"
+#region OpenShift Deployment Variables
+$OC_SECRET_PATH = "$($CRC_PATH).\secrets\"
+$OC_EAP_PROJECT = "eap-demo-03"
+$JBOSS_EAP_IMAGE_DIRECTORY = "eap73-openjdk11"
+$JBOSS_EAP_IMAGE_URL = "https://raw.githubusercontent.com/jboss-container-images/jboss-eap-7-openshift-image/$($JBOSS_EAP_IMAGE_DIRECTORY)/templates/"
+#endregion
 
 oc new-project $OC_EAP_PROJECT
 
-# Adding registry.redhat.io secret
+#region Adding registry.redhat.io secret
+
 oc create -f "$($OC_SECRET_PATH)12666202_idm-odo-sa-secret.yaml" --namespace=$OC_EAP_PROJECT
 oc create -f "$($OC_SECRET_PATH)12666202_parris-redhat-com-secret.yaml" --namespace=$OC_EAP_PROJECT
 #endregion
@@ -35,13 +55,22 @@ oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11
 oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-sso-s2i.json"
 oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-third-party-db-s2i.json"
 oc replace --force -n $OC_EAP_PROJECT -f "$($JBOSS_EAP_IMAGE_URL)eap73-openjdk11-tx-recovery-s2i.json"
+#endregion
 
-# import current OS Image from registry.redhat.io
-oc -n $OC_EAP_PROJECT import-image registry.redhat.io/jboss-eap73-openshift-rhel8:TP --confirm
+#region import current OS Image from registry.redhat.io
+
+# JBoss EAP 7
+oc -n $OC_EAP_PROJECT import-image registry.redhat.io/jboss-eap-7/eap73-openjdk11-openshift-rhel8 --confirm
+oc -n $OC_EAP_PROJECT import-image registry.redhat.io/jboss-eap-7/eap73-openjdk11-runtime-openshift-rhel8 --confirm
+oc -n $OC_EAP_PROJECT import-image registry.redhat.io/jboss-eap-7-tech-preview/eap73-openjdk11-openshift-rhel8  --confirm
+oc -n $OC_EAP_PROJECT import-image registry.redhat.io/jboss-eap-7-tech-preview/eap-cd-runtime-openshift-rhel8 --confirm
+
+# PostgreSQL 12
+oc -n $OC_EAP_PROJECT import-image registry.redhat.io/rhel8/postgresql-12 --confirm
+#endregion 
 
 <# An example Red Hat JBoss EAP 7 application. For more information about using this template, 
 see https://github.com/jboss-container-images/jboss-eap-7-openshift-image/blob/eap72/README.adoc #>
-
 
 <# commented out to prevent running
 # Install basic EAP 7.x, openJDK 11 S2I on to OpenShift
@@ -49,8 +78,8 @@ see https://github.com/jboss-container-images/jboss-eap-7-openshift-image/blob/e
 oc -n $OC_EAP_PROJECT new-app eap73-openjdk11-basic-s2i -p APPLICATION_NAME=eap73-basic-app
 
  #>
- 
 
+#region JBOSS EAP Quickstart Helloworld HTML5
 
 <# commented out to prevent running
 # Install JBOSS EAP Quickstar Hello World HTML5 with specific tag
@@ -64,3 +93,21 @@ oc new-app --template=eap73-openjdk11-basic-s2i `
  -p CONTEXT_DIR=helloworld-html5
  
  #>
+#endregion
+ 
+#region JBOSS EAP Quickstart Hibernate 
+
+ <# commented out to prevent running
+# Install JBOSS EAP Quickstart Hibernate with specific tag
+# Powershell continue next line is "`" and replaced bash "\"
+
+oc new-app --template=eap73-openjdk11-basic-s2i `
+ -p IMAGE_STREAM_NAMESPACE=$OC_EAP_PROJECT `
+ -p SOURCE_REPOSITORY_URL=https://github.com/jboss-developer/jboss-eap-quickstarts `
+ -p SOURCE_REPOSITORY_REF=7.3.x-openshift `
+ -p GALLEON_PROVISION_LAYERS=jaxrs-server `
+ -p CONTEXT_DIR=helloworld-html5
+ 
+ #>
+
+#endregion
