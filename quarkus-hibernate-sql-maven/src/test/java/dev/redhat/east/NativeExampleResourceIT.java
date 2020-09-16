a69445258bb4c25443dfd728733d0115a63d55f2
Parris Lucas <1635152+GrooveCS@users.noreply.github.com>
2020-09-16 16:33:12 -0400

    Create base start quarkus hibernate SQL (code.quarkus.redhat.com)
    Start Application (Gradle) : https://code.quarkus.redhat.com/?g=dev.redhat.east&a=quarkus-hibernate-sql&v=1.0.0-SNAPSHOT&b=GRADLE&c=dev.redhat.east.ExampleResource&s=YjV.vH0.8IG.78I.9Be.R6e.FBQ.qiu.Xkd.UAO.Bqi.pbs&cn=code.quarkus.redhat.com
    
    Start Application (Maven): https://code.quarkus.redhat.com/?g=dev.redhat.east&a=quarkus-hibernate-sql-maven&v=1.0.0-SNAPSHOT&b=MAVEN&c=dev.redhat.east.ExampleResource&s=YjV.vH0.XZs.8IG.78I.9Be.R6e.FBQ.UCw.qiu.Xkd.UAO.Bqi.pbs&cn=code.quarkus.redhat.com

---
+++ quarkus-hibernate-sql-maven/src/test/java/dev/redhat/east/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package dev.redhat.east;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
